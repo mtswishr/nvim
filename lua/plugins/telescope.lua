ef1eef7fbff2e5e91dbf9eee587e02c6dacdd297
Michael Swisher <michael.swisher@ouster.io>
2023-10-12 12:31:53 -0700

    I made a mistake

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,5 +11,7 @@ return {
                 { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
                 { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
                 { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "List current buffers" },
+                { "<leader>fs", "<cmd>Telescope lsp_dynamic_workspace_symbols <cr>", desc = "Search for symbols" },
+                { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "List git commits" },
         }
 }
